2314b0e469676a3029f45bc6ca8f7694b138bc41
kevin.chang <k457asyyu@gmail.com>
2020-05-22 06:47:48 +0000

    add cpu fwd

--- test03.cpp
+++ test03.cpp
@@ -14,13 +14,92 @@
 // COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#include <iostream>
 #include "half.hpp"
+#include <stdio.h>
 
 using half_float::half;
 
 
+static inline size_t naive_conv_out_size(size_t in_size, size_t pad, size_t dilation, size_t ksize, size_t stride)
+{
+     return (in_size + 2*pad- dilation*(ksize-1) -1)/stride + 1;
+}
+
+static inline void naive_conv_fwd_nchw(const half *src, const half *filter, half *dst,
+    size_t n, size_t w, size_t h, size_t c, size_t k, size_t fx, size_t fy, size_t px, size_t py, size_t sx, size_t sy, size_t dx, size_t dy)
+{
+    size_t in,ik,ioh,iow,ic,is,ir;
+    size_t cur_h, cur_w, o_idx, i_idx, f_idx;
+    size_t oh = naive_conv_out_size(h, py, dy, fy, sy);
+    size_t ow = naive_conv_out_size(w, px, dx, fx, sx);
+    for(in=0;in<n;in++){
+        for(ik=0;ik<k;ik++){
+            for(ioh=0;ioh<oh;ioh++){
+                for(iow=0;iow<ow;iow++){
+                    // sliding window for this filter
+                    half value(0.0);
+                    o_idx = in*k*oh*ow+ik*oh*ow+ioh*ow+iow;
+                    for(ic=0;ic<c;ic++){
+                        for(ir=0;ir<fy;ir++){
+                            cur_h = sy*ioh-py+dy*ir;
+                            if(cur_h<0 || cur_h>=h) continue;
+                            for(is=0;is<fx;is++){
+                                cur_w = sx*iow-px+dx*is;
+                                if(cur_w<0 || cur_w>=w) continue;
+                                i_idx = in*c*h*w+ic*h*w+cur_h*w+cur_w;
+                                f_idx = ik*c*fy*fx+ic*fy*fx+ir*fx+is;
+                                value += src[i_idx]*filter[f_idx];
+                            }
+                        }
+                    }
+                    dst[o_idx] = value;
+                }
+            }
+        }
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
-	half a(3.14159), b(-7), c = sin(a+b);
-	std::cout << c << ", " << ilogb(c) << '\n';
+	//half a(3.14159), b(-7), c = sin(a+b);
+    unsigned int N = 4;
+    unsigned int K = 64;
+    unsigned int C = 64;
+    unsigned int H = 14;
+    unsigned int W = 14;
+    unsigned int R = 3;
+    unsigned int S = 3;
+    unsigned int Oh = naive_conv_out_size(size_t(H), size_t(0), size_t(0), size_t(R), size_t(1));
+    unsigned int Ow = naive_conv_out_size(size_t(W), size_t(0), size_t(0), size_t(S), size_t(1));
+
+
+    unsigned int inSize = N * C* H* W* sizeof(half);
+    unsigned int weiSize = K * C* R* S* sizeof(half);
+    unsigned int outSize = N * K* Oh* Ow* sizeof(half);
+    half *in = (half *)malloc(inSize);
+    half *wei = (half *)malloc(weiSize);
+    half *out = (half *)malloc(outSize);
+
+    for (int i =0;i< N * C* H* W;i++)
+    {
+        in[i]= i ;
+    }
+
+    for (int i =0;i< K * C* R* S;i++)
+    {
+        wei[i]= i ;
+    }
+
+    for (int i =0;i< N * K* Oh* Ow;i++)
+    {
+        out[i]= i ;
+    }
+
+    naive_conv_fwd_nchw(in, wei,out, N, H, W, C, K, S, R , 0 ,0 , 1, 1, 1, 1);
+ 
+    //printf("====kevin test %f======\n",float(in[2])); 
+
+    return 0;
 }
